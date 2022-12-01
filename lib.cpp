c184c2816c1f03ea83beac02d465693b9104c6ce
giuliapucci <115046420+giuliapucci@users.noreply.github.com>
2022-12-01 15:44:15 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "primaLibreria.h"
+int n_primo(int a, int b) {
+    bool x=true;
+    if (b!=1) {
+        if ((a % b) != 0) {
+            b--;
+            x = n_primo(a, b);
+        } else {
+            x = false;
+        }
+    } else {
+        x = true;
+    }
 
+    return x;
+}
