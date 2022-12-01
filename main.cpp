cb1eafac399b774a3bbc4c233758af4415390443
giuliapucci <115046420+giuliapucci@users.noreply.github.com>
2022-12-01 15:47:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+  int main() {
+int a=0;
+cin >> a;
+int b=(a-1);
+if (n_primo(a,b)) {
+    cout << "numero primo";
+}else if (n_primo(a,b)==false) {
+    cout << "numero non primo";
+} return 0;
 }
+  
